704eb15f6b4bb98911c70ec541fa518b66cbe2d5
AllisonFlinn <allisonflinn15@gmail.com>
2024-06-15 23:11:35 -0600

    Program Complete

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percent = Console.ReadLine();
+        int grade = int.Parse(percent);
+
+        string letter = "";
+        if (grade >= 90)
+        {
+           letter = "A"; 
+        }
+        else if (grade >= 80)
+        {
+            letter = "B"; 
+        }
+        else if (grade >= 70)
+        {
+            letter = "C"; 
+        }
+        else if (grade >= 60)
+        {
+            letter = "D"; 
+        }
+        else
+        {
+           letter = "F"; 
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (grade >= 70)
+        {
+            Console.Write("Great Work! You are passing!");
+        }
+        else
+        {
+            Console.Write("Study Hard! You'll get it next time.");
+        }
     }
 }
