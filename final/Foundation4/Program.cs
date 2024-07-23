4a74bb88b2d303434d14302c3e402c90d642e726
AllisonFlinn <allisonflinn15@gmail.com>
2024-07-23 00:01:40 -0600

    Program 4 finished

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -6,13 +6,13 @@ class Program
     {
          List<Exercise> exerciseList = new List<Exercise>();
         
-        Running running = new Running("July 15, 2024", 30, 4.8);
+        Running running = new Running("July 15, 2024", 20, 2.8);
         exerciseList.Add(running);
 
-        StationaryBicycles stationaryBicycles = new StationaryBicycles("July 17, 2024", 60, 15);
+        StationaryBicycles stationaryBicycles = new StationaryBicycles("July 17, 2024", 60, 11);
         exerciseList.Add(stationaryBicycles);
 
-        Swimming swimming = new Swimming("July 24, 2024", 20, 20);
+        Swimming swimming = new Swimming("July 24, 2024", 20, 15);
         exerciseList.Add(swimming);
 
         Console.WriteLine("Exercises:");
